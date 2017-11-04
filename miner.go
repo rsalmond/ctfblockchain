345b1f261db988b198325cc8dc24463b158b74a3
Rob Salmond <rob@salmond.ca>
2017-11-03 21:47:34 -0700

    add status reporting and periodic fetching of blockchain (in case others solve it)

--- miner.go
+++ miner.go
@@ -14,13 +14,13 @@ import "encoding/json"
 import "github.com/pkg/errors"
 import (log "github.com/sirupsen/logrus")
 
-//var blockserver string = "http://192.168.0.19:5000/chain"
-var blockserver string = "http://localhost:5000/chain"
+//var blockserver string = "http://192.168.0.19:5000"
+var blockserver string = "http://localhost:5000"
 
 type Status struct {
 	Username string `json:"username"`
 	ClientId string `json:"client_id"`
-	HashRate string `json:"hash_rate"`
+	HashRate int `json:"hashrate"`
 }
 
 type Config struct {
@@ -53,7 +53,8 @@ func (b *Block) set_blockid() {
 func getChain() (blocks []Block, err error) {
 	// fetch and decode blockchain state json
 	log.Info("Getting blockchain from server.")
-	resp, err := http.Get(blockserver)
+	endpoint := fmt.Sprintf("%s/chain", blockserver)
+	resp, err := http.Get(endpoint)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to connect to blockserver: " + blockserver)
 	}
@@ -74,7 +75,8 @@ func getChain() (blocks []Block, err error) {
 func postChain(blocks_json []byte) error {
 	//TODO: some better checking on this response
 	log.Info("Posting update to blockserver.")
-	resp, err := http.Post(blockserver, "application/json", bytes.NewBuffer(blocks_json))
+	endpoint := fmt.Sprintf("%s/chain", blockserver)
+	resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(blocks_json))
 	if err != nil {
 		return errors.Wrap(err, "Failed to post updated blockchain to blockserver.")
 	}
@@ -98,7 +100,7 @@ func difficultyTarget(target int) string {
 	return target_buffer.String()
 }
 
-func mineChain(c []Block) ([]Block, error) {
+func mineChain(c []Block, hashrate_report *chan int, quit *chan bool) ([]Block, error) {
 	// mine a block on the chain until it is solved then return the chain for processing back to the server
 	var currentBlock Block
 	var currentBlock_num int
@@ -114,6 +116,7 @@ func mineChain(c []Block) ([]Block, error) {
 	// stop mining if all blocks are solved
 	if currentBlock.Difficulty == 0 {
 		return nil, errors.New("No blocks found to solve.")
+		close(*quit)
 	}
 
 	log.Info(fmt.Sprintf("Working on block number %d at difficulty rating %d", currentBlock_num, currentBlock.Difficulty))
@@ -125,6 +128,8 @@ func mineChain(c []Block) ([]Block, error) {
 	hashed := make([]byte,0)
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	hash_count := 0
+	hashes_per_sec := 0
+	report_interval := 5
 	start_time := time.Now().Unix()
 	// hash block with random nonce values until we hit a hash that has enough 0's 
 	// at the beginning to satisfy the difficulty requirement
@@ -137,10 +142,13 @@ func mineChain(c []Block) ([]Block, error) {
 			return c, nil
 		}
 		hash_count++
-		if start_time + 10 < time.Now().Unix() {
-			log.Info(fmt.Sprintf("Hashes per second: %d", hash_count / 10))
+		if start_time + int64(report_interval) < time.Now().Unix() {
+			hashes_per_sec = hash_count / report_interval
+			log.Info(fmt.Sprintf("Hashes per second: %d", hashes_per_sec))
+			*hashrate_report <- hashes_per_sec
 			hash_count = 0
 			start_time = time.Now().Unix()
+			return nil, nil
 		}
 	}
 	log.Info(fmt.Sprintf("Block: %+v\n", currentBlock))
@@ -189,14 +197,25 @@ func getConfig() (username string, clientid string, config_error error) {
 	return "", "", nil
 }
 
-func main() {
-	_, _, err := getConfig()
+func postStatus(status_json []byte) error {
+	log.Info("Posting status to blockserver.")
+	endpoint := fmt.Sprintf("%s/status", blockserver)
+	resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(status_json))
 	if err != nil {
-		fmt.Printf("%s\n", err)
-		fmt.Println("Exiting.")
-		return
+		return errors.Wrap(err, "Failed to post status to blockserver.")
 	}
 
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.Wrap(err, "Blockserver did not accept status update." + string(body))
+	}
+
+	log.Info(fmt.Sprintf("Blockserver responded: %s", string(body)))
+	return nil
+}
+
+func toil(hashrate_report *chan int, quit *chan bool) {
+	// slave away in the mines
 	for {
 		// fetch blocks
 		blockchain, err := getChain()
@@ -207,22 +226,54 @@ func main() {
 		}
 
 		// mine a block
-		newchain, err := mineChain(blockchain)
+		newchain, err := mineChain(blockchain, hashrate_report, quit)
 		if err != nil {
 			fmt.Printf("Miner exited with message: %s\n", err)
 			fmt.Println("Exiting.")
 			return
 		}
 
-		// json encode mined block
-		json_chain, err := json.Marshal(newchain);
-		if  err != nil {
-			panic(err)
-		}
+		if newchain != nil {
+			// json encode mined block
+			json_chain, err := json.Marshal(newchain);
+			if  err != nil {
+				panic(err)
+			}
 
-		// post mined block to blockserver
-		if err := postChain(json_chain); err != nil {
+			// post mined block to blockserver
+			if err := postChain(json_chain); err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func reportStatus(username string, client_id string, hashrate_report *chan int) {
+	// send a status report every time the miner writes current hashrate to the report channel
+	for {
+		status := Status{}
+		status.Username = username
+		status.ClientId = client_id
+		status.HashRate = <-*hashrate_report
+		status_data, _ := json.Marshal(status)
+
+		if err := postStatus(status_data); err != nil {
 			panic(err)
 		}
 	}
+}
+
+func main() {
+	username, client_id, err := getConfig()
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		fmt.Println("Exiting.")
+		return
+	}
+
+	hashrate_report := make(chan int)
+	quit := make(chan bool)
+	go toil(&hashrate_report, &quit)
+	go reportStatus(username, client_id, &hashrate_report)
+	select {}
 }
